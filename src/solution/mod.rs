bac7082f4924e433069163ab166334f6ff6a4b16
zyleo@wsl2-ubuntu20.10 <1872405512@qq.com>
2021-12-30 23:46:36 +0800

    独立完成了n0027，虽说简单，但也算Rust学习之路上的进步

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1,3 +1,4 @@
 pub mod n0001_two_sum;
 pub mod n0002_add_two_numbers;
 pub mod n0026_remove_duplicates_from_sorted_array;
+pub mod n0027_remove_elements;
---
+++ src/solution/n0027_remove_elements.rs
@@ -0,0 +1,28 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let len = nums.len();
+        let mut slow: usize = 0;
+        let mut index: usize = 0;    
+        while index < len {
+            if nums[index] != val {
+                nums[slow] = nums[index];
+                slow += 1;
+            }
+            index += 1;
+        }
+        slow as i32
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_0027() {
+        assert_eq!(Solution::remove_element(&mut(vec![2,3,3,1,5,3]), 3), 3);
+    }
+}
